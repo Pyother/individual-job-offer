e2fe9bcbb0b128d31719cb71a16561df6153ed29
piotr <piotr.dominik.sobol@gmail.com>
2022-12-02 00:35:25 +0100

    Setting layout of form input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
 import FormInput from "./components/FormInput";
 import "./App.css";
 
 export default function App() {
     return (
-        <div>
-            <FormInput />
-        </div>
+        <form>
+            <FormInput placeholder="Pracodawca"/>
+            <FormInput placeholder="System wynagradzania"/> 
+            <FormInput placeholder="Imię"/>
+            <FormInput placeholder="Nazwisko"/>
+            <FormInput placeholder="Płeć"/>
+            <FormInput placeholder="Stawka"/>
+            <FormInput placeholder="Data zatrudnienia"/>
+            <FormInput placeholder="Dodatek funkcyjny"/>
+            <FormInput placeholder="Imię rekrutera"/>
+            <FormInput placeholder="Nazwisko rekrutera"/>
+            <FormInput placeholder="E-mail rekrutera"/>
+        </form>
     );
 }
 
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,12 +1,14 @@
 import props from 'prop-types';
 import './formInput.css';
 
-const FormInput = () => {
+const FormInput = (props) => {
     return (
         <div className="formInput">
+            <h2>{props.placeholder}</h2>
             <input placeholder={props.placeholder}/>
         </div>
     )
 }
 
 export default FormInput
+
