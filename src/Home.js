accd9bf454673a5835372cfb3ad3c4babd72b614
piotr <piotr.dominik.sobol@gmail.com>
2022-12-09 15:12:33 +0100

    Add header shrinking effect

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,23 @@
 import './App.css';
 
+window.addEventListener('scroll', function() {
+    const scroolElement = this.document.getElementById("header-container");
+    this.document.getElementById("header-1").style = "position: sticky; top: 50%; transform: translateY(0, -50%);";
+});
+
 export default function Home() {
     return (
         <div className="homepage">
-            <div className="headercontainer">
-                <h4>Kalkulator </h4>
-                <div className="firstheader">Indywidualnej</div>
-                <div className="secondheader">Propozycji</div>
-                <div className="thirdheader">Zatrudnienia</div>
+            <div className="header-container">
+                <div className="title-wrapper-1">
+                    <h1 id='header-1'>Kalkulator</h1>
+                </div>
+                <div className="title-wrapper-2">
+                    
+                </div>
+            </div>
+            <div className="content-container">
+
             </div>
         </div>
     );
