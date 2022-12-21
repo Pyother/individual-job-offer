27be24737e60f7e9c6268eac7b2c646fcfd72ef9
piotr <piotr.dominik.sobol@gmail.com>
2022-12-21 18:21:39 +0100

    Define dtyle of guide-content

--- src/components/HomePageGuide.jsx
+++ src/components/HomePageGuide.jsx
@@ -13,20 +13,29 @@ const HomePageGuide = (props) => {
                     </div>
                 </Row>
                 <p>(Miejsce na opis i przeznaczenie strony)</p>
-                <Row style={{padding: "10%"}}>
+                <Row style={{padding: "5%", width: "1200px"}}>
                     <Col md={4}>
                         <div className="guide-content">
-                            1
+                            <Col>
+                                <Row><h1>Baza danych</h1></Row>
+                                <Row><p>(Opis)</p></Row>
+                            </Col>
                         </div>
                     </Col>
                     <Col md={4}>
                         <div className="guide-content">
-                            2
+                            <Col>
+                                <Row><h1>Formularz</h1></Row>
+                                <Row><p>(Opis formularza)</p></Row>
+                            </Col>
                         </div>
                     </Col>
                     <Col md={4}>
                         <div className="guide-content">
-                            3
+                            <Col>
+                                <Row><h1>Panel administracyjny</h1></Row>
+                                <Row><p>(Opis zastosowania panelu administracyjnego)</p></Row>
+                            </Col>
                         </div>
                     </Col>
                 </Row>
